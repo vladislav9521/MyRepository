cbd3bc1f88cf2cd1a0815fd335edbf6357bfdb6b
vladislav9521 <vladislav.95.21@yandex.by>
2022-07-17 08:30:36 +0300

    Drink Factory
    Adding
    1. products: abstract bottle, cap, liquid
    2. factories: abstract  factory
    3. and their concrete implementation
    4. Client code. Products interaction

---
+++ .NET/Design Pattrns/Abstract Factory/DrinkFactory/DrinkFactory/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkFactory
+{
+    class Client
+    {
+
+    AbstractLiquid liquid = null;
+    AbstractCap cap = null;
+    AbstractBottle bottle = null;
+
+    public Client(AbstractDrinkFactory drinkFactory)
+    {
+        liquid = drinkFactory.GetLiquid();
+        cap = drinkFactory.GetCap();
+        bottle = drinkFactory.GetBottle();
+    }
+
+    public void Interact()
+    {
+        bottle.ScrewCap(cap);
+        bottle.PourLiquid(liquid);
+    }
+    }
+ 
+}
